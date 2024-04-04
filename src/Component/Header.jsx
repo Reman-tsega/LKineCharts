5c2f2ba31768ef12d427e78ec194565a81f84cd1
Reman-tsega <remantsga@gmail.com>
2024-04-04 12:49:57 +0300

    header

--- src/Component/Header.jsx
+++ src/Component/Header.jsx
@@ -7,9 +7,15 @@ function Header() {
 
   return (
     <div className={`px-4 md:px-20 lg:px-32 mt-10 items-center ${darkMode ? 'text-white bg-black' : 'text-black bg-white'}`}>
-      <h1 className="text-center text-4xl">{darkMode ? 'Line Chart' : 'Line Chart'}</h1>
+      <h1 className="text-center text-4xl">Line Chart</h1>
       <div className='flex justify-end'>
         <DownloadSourceCode />
+      </div>
+      <div className="graphic-header p-6 shadow-md z-10 flex">
+        <h2 className={`graphic-headline m-0 text-2xl font-semibold flex justify-between gap-6 ${darkMode ? 'text-white' : 'text-black'}`}>
+          <img className="graphic-headline-icon max-w-8 max-h-8 bg-white" src="https://assets.graphic-portal.com/spaces/-Ns4Y6W9ApHcBTBEzu9S/graphics/-NsNham4iKudej0xLINo/versions/-NsNhi7ANB5ZktQHydB2/main/bundestag.png" alt="icon" />
+          Sonntagsfragen zur Bundestagswahl
+        </h2>
       </div>
     </div>
   );
